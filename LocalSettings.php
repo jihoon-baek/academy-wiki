1d62bdf2008305d6a41292dac40b4fa4802831b1
백지훈 <itsjohnbaek@baegjihun-ui-MacBookPro.local>
2026-03-10 22:29:42 +0900

    Change default skin to vector-2022 for Namuwiki styling

--- LocalSettings.php
+++ LocalSettings.php
@@ -129,7 +129,7 @@ $wgGroupPermissions["*"]["edit"] = false;
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
-$wgDefaultSkin = "vector";
+$wgDefaultSkin = "vector-2022";
 
 # Enabled skins.
 # The following skins were automatically enabled:
